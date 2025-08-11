3697e05b3ee7011e285b1374836e80b09b00e359
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2025-08-11 13:08:32 +0200

    Patch mail property on user if user account already exists on a previous UPN/mail where mail is present in proxyAddresses

--- src/lib/entra-functions.ts
+++ src/lib/entra-functions.ts
@@ -4,6 +4,11 @@ import { HTTPError } from "./HTTPError";
 
 const scope = "https://graph.microsoft.com/.default"
 
+type InvitedUser = {
+  id: string
+  patched: boolean
+}
+
 const getGraphHeaders: (scope: string) => Promise<HeadersInit> = async (scope: string): Promise<HeadersInit> => {
   const entraIdToken: string = await getEntraIdToken(scope)
 
@@ -40,8 +45,7 @@ async function getGroupIdByDisplayName(groupName: string): Promise<string> {
 }
 
 async function getUserIdByMail(userMail: string): Promise<string> {
-  const upn = encodeURIComponent(`${userMail.replace('@', '_')}#EXT#@samhandling.onmicrosoft.com`)
-  const url = `https://graph.microsoft.com/v1.0/users/${upn}?$select=id,displayName`
+  const url = `https://graph.microsoft.com/v1.0/users?$filter=mail eq '${encodeURIComponent(userMail)}'&$select=id`
   const headers: HeadersInit = await getGraphHeaders(scope)
 
   const response: Response = await fetch(url, {
@@ -59,10 +63,35 @@ async function getUserIdByMail(userMail: string): Promise<string> {
     throw new HTTPError(404, `User with mail '${userMail}' not found`)
   }
 
-  return data.id
+  if (data.value.length === 0) {
+    throw new HTTPError(404, `User with mail '${userMail}' not found`)
+  }
+
+  if (data.value.length > 1) {
+    throw new HTTPError(500, `Multiple users found with mail '${userMail}'`)
+  }
+
+  return data.value[0].id
 }
 
-async function inviteUserByMail(userMail: string, displayName: string): Promise<string> {
+async function getUserById(userId: string): Promise<any> {
+  const url = `https://graph.microsoft.com/v1.0/users/${userId}?$select=id,mail,proxyAddresses`
+  const headers: HeadersInit = await getGraphHeaders(scope)
+
+  const response: Response = await fetch(url, {
+    method: "GET",
+    headers
+  })
+
+  if (!response.ok) {
+    const errorData = await response.json()
+    throw new HTTPError(response.status, `Failed to fetch user by id: ${response.statusText} - ${JSON.stringify(errorData, null, 2)}`)
+  }
+
+  return response.json()
+}
+
+async function inviteUserByMail(userMail: string, displayName: string): Promise<InvitedUser> {
   const url = `https://graph.microsoft.com/v1.0/invitations`
   const headers: HeadersInit = await getGraphHeaders(scope)
 
@@ -86,7 +115,42 @@ async function inviteUserByMail(userMail: string, displayName: string): Promise<
 
   const data: any = await response.json()
   logger('info', [`Invited user with display name '${data.invitedUserDisplayName}', email '${data.invitedUserEmailAddress}' and id '${data.invitedUser.id}'`, data])
-  return data.invitedUser.id
+
+  const user = await getUserById(data.invitedUser.id)
+  let patched = false
+  if (user.mail.toLowerCase() !== userMail.toLowerCase()) {
+    logger('info', [`Will patch user with id '${data.invitedUser.id}' to have mail '${userMail}'`, user])
+    patched = await patchUser(data.invitedUser.id, userMail)
+  }
+
+  return {
+    id: data.invitedUser.id,
+    patched
+  }
+}
+
+async function patchUser(userId: string, userMail: string): Promise<boolean> {
+  const url = `https://graph.microsoft.com/v1.0/users/${userId}`
+  const headers: HeadersInit = await getGraphHeaders(scope)
+
+  const body = JSON.stringify({
+    mail: userMail
+  })
+
+  const response: Response = await fetch(url, {
+    method: "PATCH",
+    headers,
+    body
+  })
+
+  if (!response.ok) {
+    const errorData = await response.json()
+    throw new HTTPError(response.status, `Failed to patch user: ${response.statusText} - ${JSON.stringify(errorData, null, 2)}`)
+  }
+
+  logger('warn', [`Patched user with id '${userId}' to have mail '${userMail}'`])
+
+  return true
 }
 
 export async function listGroupMembers(groupName: string, allowedUpnSuffixes: string[]): Promise<string[]> {
@@ -113,11 +177,14 @@ export async function addGroupMember(groupName: string, userMail: string, displa
   const groupId: string = await getGroupIdByDisplayName(groupName)
 
   let userId: string
+  let patched: boolean = false
   try {
     userId = await getUserIdByMail(userMail)
   } catch (error) {
     if (error instanceof HTTPError && error.status === 404) {
-      userId = await inviteUserByMail(userMail, displayName)
+      const invitedUser: InvitedUser = await inviteUserByMail(userMail, displayName)
+      userId = invitedUser.id
+      patched = invitedUser.patched
     } else {
       throw error
     }
@@ -138,6 +205,10 @@ export async function addGroupMember(groupName: string, userMail: string, displa
 
   if (!response.ok) {
     const errorData = await response.json()
+    if (patched && errorData.error.message.includes('already exist')) {
+      return 204
+    }
+
     throw new HTTPError(response.status, `Failed to add group member: ${response.statusText} - ${JSON.stringify(errorData, null, 2)}`)
   }
 
