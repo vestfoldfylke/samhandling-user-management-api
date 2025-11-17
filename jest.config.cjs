3dfad5b8819e534e3a7c19d10eb1f5c43896271c
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2025-11-17 15:03:29 +0100

    chore: Moved from `jest` to `node:test`

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import("jest").Config} **/
-module.exports = {
-  testEnvironment: "node",
-  roots: ["<rootDir>/dist_tests"],
-  testMatch: ["**/tests/**/*.test.js"],
-  transform: {},
-  moduleNameMapper: {}
-}
--- tests/county-validation.test.ts
+++ tests/county-validation.test.ts
@@ -1,3 +1,5 @@
+import assert from 'node:assert'
+import { describe, it } from 'node:test'
 import { HttpRequest, InvocationContext } from '@azure/functions'
 
 import { countyValidation } from '../src/lib/county-validation.js'
@@ -36,73 +38,57 @@ const mockContext: InvocationContext = {
 }
 
 describe('countyValidation should throw an error when', (): void => {
-  test('headerKey and queryKey are missing', (): void => {
-    expect((): void => {
-      countyValidation(mockRequest, mockContext)
-    }).toThrow('Unauthorized: Missing security key in header or query string')
+  it('headerKey and queryKey are missing', (): void => {
+    assert.throws((): string[] => countyValidation(mockRequest, mockContext))
   })
 
-  test('headerKey is found but is invalid', (): void => {
+  it('headerKey is found but is invalid', (): void => {
     const invalidHeaderKeyRequest: HttpRequest = {
       ...mockRequest,
       headers: new Headers({ 'X-County-Key': 'invalidKey' })
     }
 
-    expect((): void => {
-      countyValidation(invalidHeaderKeyRequest, mockContext)
-    }).toThrow('Unauthorized: Invalid security key in header or query string')
+    assert.throws((): string[] => countyValidation(invalidHeaderKeyRequest, mockContext))
   })
 
-  test('queryKey is found but is invalid', (): void => {
+  it('queryKey is found but is invalid', (): void => {
     const invalidQueryKeyRequest: HttpRequest = {
       ...mockRequest,
       query: new URLSearchParams({ 'countyKey': 'invalidKey' })
     }
 
-    expect((): void => {
-      countyValidation(invalidQueryKeyRequest, mockContext)
-    }).toThrow('Unauthorized: Invalid security key in header or query string')
+    assert.throws((): string[] => countyValidation(invalidQueryKeyRequest, mockContext))
   })
 
-  test('headerKey is found and valid but specified mail is not in allowed upn suffixes', (): void => {
+  it('headerKey is found and valid but specified mail is not in allowed upn suffixes', (): void => {
     const key: string = 'validKey'
     const mail: string = ' foo@bar.nO ' // intentionally using mixed case and spaces to test trimming and case insensitivity
-    const expectedMail: string = mail.trim().toLowerCase()
-    const allowedUpnSuffixes: string = 'biZ.no, example.com' // intentionally using mixed case and spaces to test trimming and case insensitivity
-    const expectedSuffixes: string[] = allowedUpnSuffixes.split(',').map(suffix => suffix.trim().toLowerCase())
-    process.env[key] = allowedUpnSuffixes
+    process.env[key] = 'biZ.no, example.com' // intentionally using mixed case and spaces to test trimming and case insensitivity
 
     const validMockKeyRequest: HttpRequest = {
       ...mockRequest,
       headers: new Headers({ 'X-County-Key': key })
     }
 
-    expect((): void => {
-      countyValidation(validMockKeyRequest, mockContext, mail)
-    }).toThrow(`Forbidden: User mail (${expectedMail}) does not match any of the allowed UPN suffixes: [${expectedSuffixes.join(', ')}]`)
+    assert.throws((): string[] => countyValidation(validMockKeyRequest, mockContext, mail))
   })
 
-  test('queryKey is found and valid but specified mail is not in allowed upn suffixes', (): void => {
+  it('queryKey is found and valid but specified mail is not in allowed upn suffixes', (): void => {
     const key: string = 'validKey'
     const mail: string = ' foo@bar.nO ' // intentionally using mixed case and spaces to test trimming and case insensitivity
-    const expectedMail: string = mail.trim().toLowerCase()
-    const allowedUpnSuffixes: string = 'biZ.no, example.com' // intentionally using mixed case and spaces to test trimming and case insensitivity
-    const expectedSuffixes: string[] = allowedUpnSuffixes.split(',').map(suffix => suffix.trim().toLowerCase())
-    process.env[key] = allowedUpnSuffixes
+    process.env[key] = 'biZ.no, example.com' // intentionally using mixed case and spaces to test trimming and case insensitivity
 
     const validMockKeyRequest: HttpRequest = {
       ...mockRequest,
       query: new URLSearchParams({ 'countyKey': key })
     }
 
-    expect((): void => {
-      countyValidation(validMockKeyRequest, mockContext, mail)
-    }).toThrow(`Forbidden: User mail (${expectedMail}) does not match any of the allowed UPN suffixes: [${expectedSuffixes.join(', ')}]`)
+    assert.throws((): string[] => countyValidation(validMockKeyRequest, mockContext, mail))
   })
 })
 
 describe('countyValidation should return allowed upn suffixes when', (): void => {
-  test('headerKey is found and valid and mail is not specified', (): void => {
+  it('headerKey is found and valid and mail is not specified', (): void => {
     const key: string = 'validKey'
     process.env[key] = 'baR.nO, example.com' // intentionally using mixed case and spaces to test trimming and case insensitivity
 
@@ -112,11 +98,11 @@ describe('countyValidation should return allowed upn suffixes when', (): void =>
     }
 
     const expectedSuffixes: string[] = countyValidation(validMockKeyRequest, mockContext)
-    expect(Array.isArray(expectedSuffixes)).toBeTruthy()
-    expect(expectedSuffixes).toEqual(['bar.no', 'example.com'])
+    assert.strictEqual(Array.isArray(expectedSuffixes), true)
+    assert.deepStrictEqual(expectedSuffixes, ['bar.no', 'example.com'])
   })
 
-  test('headerKey is found and valid and mail is specified and in allowed upn suffixes', (): void => {
+  it('headerKey is found and valid and mail is specified and in allowed upn suffixes', (): void => {
     const key: string = 'validKey'
     const mail: string = ' foo@bar.nO ' // intentionally using mixed case and spaces to test trimming and case insensitivity
     process.env[key] = 'baR.no, example.com' // intentionally using mixed case and spaces to test trimming and case insensitivity
@@ -127,11 +113,11 @@ describe('countyValidation should return allowed upn suffixes when', (): void =>
     }
 
     const expectedSuffixes: string[] = countyValidation(validMockKeyRequest, mockContext, mail)
-    expect(Array.isArray(expectedSuffixes)).toBeTruthy()
-    expect(expectedSuffixes).toEqual(['bar.no', 'example.com'])
+    assert.strictEqual(Array.isArray(expectedSuffixes), true)
+    assert.deepStrictEqual(expectedSuffixes, ['bar.no', 'example.com'])
   })
 
-  test('queryKey is found and valid and mail is not specified', (): void => {
+  it('queryKey is found and valid and mail is not specified', (): void => {
     const key: string = 'validKey'
     process.env[key] = ' example.com, baR.no ' // intentionally using mixed case and spaces to test trimming and case insensitivity
 
@@ -141,11 +127,11 @@ describe('countyValidation should return allowed upn suffixes when', (): void =>
     }
 
     const expectedSuffixes: string[] = countyValidation(validMockKeyRequest, mockContext)
-    expect(Array.isArray(expectedSuffixes)).toBeTruthy()
-    expect(expectedSuffixes).toEqual(['example.com', 'bar.no'])
+    assert.strictEqual(Array.isArray(expectedSuffixes), true)
+    assert.deepStrictEqual(expectedSuffixes, ['example.com', 'bar.no'])
   })
-  
-  test('queryKey is found and valid and mail is specified and in allowed upn suffixes', (): void => {
+
+  it('queryKey is found and valid and mail is specified and in allowed upn suffixes', (): void => {
     const key: string = 'validKey'
     const mail: string = ' foo@bar.nO ' // intentionally using mixed case and spaces to test trimming and case insensitivity
     process.env[key] = ' example.com, baR.no ' // intentionally using mixed case and spaces to test trimming and case insensitivity
@@ -156,7 +142,7 @@ describe('countyValidation should return allowed upn suffixes when', (): void =>
     }
 
     const expectedSuffixes: string[] = countyValidation(validMockKeyRequest, mockContext, mail)
-    expect(Array.isArray(expectedSuffixes)).toBeTruthy()
-    expect(expectedSuffixes).toEqual(['example.com', 'bar.no'])
+    assert.strictEqual(Array.isArray(expectedSuffixes), true)
+    assert.deepStrictEqual(expectedSuffixes, ['example.com', 'bar.no'])
   })
 })
